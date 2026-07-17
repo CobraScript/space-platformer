b5faf83733e20d79e6ecb47a8071d3567dee0f7b
apple-head-coder <aarongx19@gmail.com>
2026-07-17 15:13:11 -0400

    Added crosshair and raycast

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody3D
 
 
-@export var sensitivity = 0.002
+@export var sensitivity: float = 0.002
 
-@onready var camera = $Camera3D
+@onready var camera: Camera3D = $Camera3D
+@onready var ray: RayCast3D = $Camera3D/RayCast3D
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta: float) -> void:
+	if ray.is_colliding():
+		print("raycast collision")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pointer_lock_enter"):
