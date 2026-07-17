12315b35ad22d954629f255c54004a666877df1f
apple-head-coder <aarongx19@gmail.com>
2026-07-17 11:03:57 -0400

    Scene setup and player with mouse controls

---
+++ scripts/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+
+@export var sensitivity = 0.002
+
+@onready var camera = $Camera3D
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pointer_lock_enter"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if event.is_action_pressed("pointer_lock_exit"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if event is InputEventMouseMotion \
+	and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotation.y += -event.relative.x * sensitivity
+		camera.rotation.x = clampf(
+			camera.rotation.x - event.relative.y * sensitivity,
+			-PI / 2, PI / 2
+		)
